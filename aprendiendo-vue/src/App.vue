c4a3d61067d9526a066ca9e410df87cc78a33405
juanframart2011 <juanframart2011@gmail.com>
2022-01-16 16:55:15 -0600

    upload project

---
+++ aprendiendo-vue/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MyComponent></MyComponent>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import MyComponent from './components/MyComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    MyComponent
+  }
+}
+</script>
+
+<style>
+@import './assets/css/styles.css';
+</style>
---
+++ aprendiendo-vue/src/components/MyComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="MyComponent">
+    <hr />
+    <h1>Este es mi componente</h1>
+    <h2>{{ subtitulo }}</h2>
+    <hr />
+  </div>
+</template>
+<script>
+export default {
+  name: "MyComponent",
+  data() {
+    return {
+      subtitulo: "Soy el subtitulo de mi componente",
+    };
+  },
+};
+</script>
